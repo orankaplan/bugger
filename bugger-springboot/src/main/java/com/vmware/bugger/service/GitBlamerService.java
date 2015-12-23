6819659f915c32525d12981f13ead92b36c56036
Oran Kaplan <okaplan@vmware.com>
2015-12-23 16:43:46 +0200

    Service

--- bugger-springboot/src/main/java/com/vmware/bugger/service/GitBlamerService.java
+++ bugger-springboot/src/main/java/com/vmware/bugger/service/GitBlamerService.java
@@ -2,12 +2,14 @@ package com.vmware.bugger.service;
 
 import com.vmware.bugger.model.Culprit;
 import com.vmware.bugger.model.ErrorStack;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 /**
  * Created by abadyan on 23/12/15.
  */
+@Service
 public class GitBlamerService {
     public List<Culprit> blame(ErrorStack errorStack) {
         return null;
