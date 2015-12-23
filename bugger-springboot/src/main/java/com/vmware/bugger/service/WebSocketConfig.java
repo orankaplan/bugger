7f482b911b901bd01c6fd89295ffcae953d1dfd7
Oran Kaplan <okaplan@vmware.com>
2015-12-23 23:37:01 +0200

    package

--- bugger-springboot/src/main/java/com/vmware/bugger/service/WebSocketConfig.java
+++ bugger-springboot/src/main/java/com/vmware/bugger/service/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package hello;
+package com.vmware.bugger.service;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
